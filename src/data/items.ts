7110ead46f9a87356eb1e4141aa086f6148fd530
Gustavo Henrique <64738844+gustavohenri316@users.noreply.github.com>
2021-10-11 23:52:24 -0300

    first

---
+++ src/data/items.ts
@@ -0,0 +1,8 @@
+import { Item  } from "../types/Item";
+
+export const items: Item[] = [
+    { date: new Date(2021, 9, 12), category: 'food', title: 'McDonalds', value: 32.12 },
+    { date: new Date(2021, 9, 12), category: 'food', title: 'Burguer King', value: 28 },
+    { date: new Date(2021, 9, 16), category: 'rent', title: 'Aluguel', value: 2300 },
+    { date: new Date(2021, 11, 18), category: 'salary', title: 'Salario', value: 4500 }
+];
