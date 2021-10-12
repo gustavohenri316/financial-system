2f2865d5a6954d9df43a859e4a5abdfb748854dd
Gustavo Henrique <64738844+gustavohenri316@users.noreply.github.com>
2021-10-12 10:28:18 -0300

    filtros

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { categories } from './data/categories';
 import { items } from './data/items';
 import { getCurrentMonth, filterListByMonth } from './helpers/dateFilter';
 import { TableArea } from './components/TableArea';
+import { InfoArea } from './components/InfoArea';
 
 const App = () => {
   const [list, setList] = useState(items);
@@ -15,12 +16,22 @@ const App = () => {
   useEffect(() =>{
     setFilteredList( filterListByMonth(list, currentMonth))
   }, [list, currentMonth])
+
+  const handleMonthChange = (newMonth: string) => {
+    setCurrentMonth(newMonth)
+
+  }
+
   return (
     <C.Container>
       <C.Header>
         <C.HeaderText>Sistema Financeiro</C.HeaderText>
       </C.Header>
       <C.Body>
+        <InfoArea 
+        currentMonth={currentMonth}
+        onMonthChange={handleMonthChange}
+        />
         <TableArea list={filteredList}/>
       </C.Body>
     </C.Container>
